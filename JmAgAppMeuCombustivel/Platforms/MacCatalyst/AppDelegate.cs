f3f7aad9e777178825de12ab50ebf251a53730e5
Usuario <Usuario@CPD02-M03>
2024-02-26 15:11:20 -0300

    Adicionar arquivos de projeto.

---
+++ JmAgAppMeuCombustivel/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace JmAgAppMeuCombustivel
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
